76d3dfbc0f1970403c790321af89ba8f6b3f3e08
fenghongbin <tt_fhb@163.com>
2022-03-31 23:11:06 +0800

    这是master第三次提交

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -1,10 +1,11 @@
 package cn.tt.git.controller;
 
+import java.util.Scanner;
+
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git dev");
         System.out.println("hello git test分支");
-        System.out.println("hello git dev ");
     }
 }
